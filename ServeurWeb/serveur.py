da55119eae33760c0c3c63ab5ae759694275020b
Antoine <antoine.didier@efrei.net>
2020-11-11 16:30:37 +0100

    Creation fichiers html + lien Flask

--- ServeurWeb/serveur.py
+++ ServeurWeb/serveur.py
@@ -4,9 +4,25 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def index():
     a = 3 + 4
-    html = render_template('index.html', a = a)
+    html = render_template('index.html', a=a)
     return html
 
-app.run(host='localhost', debug=True)
+@app.route('/connexion')
+def connexion():
+    html = render_template('connexion.html')
+    return html
+
+@app.route('/client',  methods=['POST'])
+def client():
+    email = request.form['email']
+    html = render_template('client.html', email=email)
+    return html
+
+@app.route('/inscription')
+def inscription():
+    html = render_template('inscription.html')
+    return html
+
+app.run()(host='localhost', debug=True)
