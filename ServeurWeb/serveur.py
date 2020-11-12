2c615797e1b6f78b4bf92ad74e8feeb7647b8c61
Antoine <antoine.didier@efrei.net>
2020-11-12 17:00:11 +0100

    Ajout et maj de fichiers

--- ServeurWeb/serveur.py
+++ ServeurWeb/serveur.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 
+from connect import connect  # import de la fonction connection
+
 app = Flask(__name__)
 
 
@@ -9,20 +11,28 @@ def index():
     html = render_template('index.html', a=a)
     return html
 
+
 @app.route('/connexion')
 def connexion():
     html = render_template('connexion.html')
     return html
 
-@app.route('/client',  methods=['POST'])
+
+@app.route('/client', methods=['POST'])
 def client():
     email = request.form['email']
+    password = request.form['password']
+    #if not connect(email, password):
+    #    ErrorHtml = render_template('index.html')
+    #    return ErrorHtml
     html = render_template('client.html', email=email)
     return html
+
 
 @app.route('/inscription')
 def inscription():
     html = render_template('inscription.html')
     return html
+
 
 app.run()(host='localhost', debug=True)
