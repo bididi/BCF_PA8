590b43a8e4638d903bb4cf019ce9d73bc80f1ace
Antoine <antoine.didier@efrei.net>
2021-02-10 23:19:53 +0100

    blueprint inscription/connexion

--- ServeurWeb/__init__.py
+++ ServeurWeb/__init__.py
@@ -1,12 +1,19 @@
+# fichier python qui permet de créer le site en local avec la redirection de toutes les pages
+#
+#  EXECUTER LE CODE :
+# dans le terminal,
+#   set FLASK_APP=__init__.py
+
 import os
 from flask import Flask, render_template, request, redirect, url_for
+
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        DATABASE=os.path.join("./", 'flaskr.sqlite'),
     )
 
     if test_config is None:
@@ -22,9 +29,13 @@ def create_app(test_config=None):
     except OSError:
         pass
 
+    # on appelle la méthode d'initialisation dans db.py pour créer les tables de la db
     from . import db
     db.init_app(app)
 
+
+
+    # ------------------------- ROUTES VERS LES PAGES DU SITE ------------------------
     @app.route('/')
     def index():
         html = render_template('index.html')
@@ -50,5 +61,12 @@ def create_app(test_config=None):
     @app.errorhandler(404)
     def page_not_found(error):
         return render_template('page_not_found.html'), 404
+
+    # --------------------------------------------------------------------
+
+    #
+
+    from . import auth
+    app.register_blueprint(auth.bp)
 
     return app
